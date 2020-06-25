031cd533a4fcf23effd0ae0c91ef689be7655874
Mitch Lum <mklum88@gmail.com>
2020-06-24 18:54:35 -0700

    moved calendar to subheader and put it in a dialog

--- components/Subheader.js
+++ components/Subheader.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -6,42 +7,98 @@ import Button from '@material-ui/core/Button'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft'
 import Sidebar from './Sidebar'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Dialog from '@material-ui/core/Dialog'
 import Calendar from 'react-calendar'
+import Grid from '@material-ui/core/Grid'
 
-const useStyles = makeStyles(theme => ({
+const emails = ['username@gmail.com', 'user02@gmail.com']
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
-    color: '#FFF',
-  },
-  date: {
-    margin: ' 0 auto',
-  },
-  login: {
-    marginRight: '16px',
   },
   toolbar: {
     minHeight: '6vh',
     backgroundColor: '#5c5c5c',
+    textAlign: 'center',
+    color: '#FFF',
   },
 }))
 
+function SimpleDialog(props) {
+  const { onClose, selectedValue, open } = props
+
+  const handleClose = () => {
+    onClose(selectedValue)
+  }
+
+  const [state, setState] = useState({ date: new Date() })
+
+  const onChange = function (date) {
+    setState({ date })
+  }
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="simple-dialog-title"
+      open={open}
+    >
+      <DialogTitle id="simple-dialog-title" style={{ textAlign: 'center' }}>
+        Select a date
+      </DialogTitle>
+      <Calendar onChange={onChange} value={state.date} />
+    </Dialog>
+  )
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+}
+
 export default function Subheader() {
   const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const [selectedValue, setSelectedValue] = React.useState(emails[1])
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = value => {
+    setOpen(false)
+    setSelectedValue(value)
+  }
 
   return (
     <div className={classes.root}>
       <Toolbar className={classes.toolbar}>
-        <Sidebar />
-        <Typography className={classes.date}>
-          <Button color="inherit">
-            <KeyboardArrowLeftIcon />
-          </Button>
-          <Button color="inherit">Monday, June 22</Button>
-          <Button color="inherit">
-            <KeyboardArrowRightIcon />
-          </Button>
-        </Typography>
-        <Typography>Weekly</Typography>
+        <Grid container spacing={3}>
+          <Grid item sm={2}>
+            <Sidebar />
+          </Grid>
+          <Grid item sm={8}>
+            <Button color="inherit">
+              <KeyboardArrowLeftIcon />
+            </Button>
+            <Button color="inherit" onClick={handleClickOpen}>
+              Monday, June 22
+            </Button>
+            <SimpleDialog
+              selectedValue={selectedValue}
+              open={open}
+              onClose={handleClose}
+            />
+            <Button color="inherit">
+              <KeyboardArrowRightIcon />
+            </Button>
+          </Grid>
+          <Grid item sm={2}>
+            <Button color="inherit">Weekly</Button>
+          </Grid>
+        </Grid>
       </Toolbar>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,8 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Subheader from '../components/Subheader'
 import Layout from '../components/Layout'
-import Calendar from 'react-calendar'
 
 export default function Home() {
-  const [state, setState] = useState({ date: new Date() })
-
-  const onChange = function (date) {
-    setState({ date })
-  }
-
   return (
     <div>
       <Head>
@@ -19,9 +12,7 @@ export default function Home() {
       </Head>
       <Header />
       <Subheader />
-      <Layout>
-        <Calendar onChange={onChange} value={state.date} />
-      </Layout>
+      <Layout />
     </div>
   )
 }
