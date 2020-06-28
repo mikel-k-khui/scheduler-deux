169a3d5a756d07c1d0da6914b79564495beef59d
Mike Chui <7028879+mikel-k-khui@users.noreply.github.com>
2020-06-27 20:22:23 -0600

    fix theme, added index.js to components and state reducer draft (#4)

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as Daily } from './Daily'
+export { default as Header } from './Header'
+export { default as Layout } from './Layout'
+export { default as Sidebar } from './Sidebar'
+export { default as Subheader } from './Subheader'
+export { default as theme } from './theme'
---
+++ state/slotContext.js
@@ -0,0 +1,26 @@
+// import { createContext } from 'react'
+// import {
+//   SET_DATE,
+//   ADD_FILTER,
+//   CLEAR_FILTER,
+//   DAILY_VIEW,
+//   WEEKLY_VIEW
+// } from './constant'
+
+// const slotContext = createContext()
+
+// function useLayoutContext() {
+//   const stateContext
+// }
+
+// function slotReducer(state, { type, payload }) => {
+//   const types = {
+//     SET_DATE: { ...state, dateDisplayed: payload.dateDisplayed },
+//     ADD_FILTER: ((state, payload) => ({ ...state, resourceFilter: payload.resourceFilter })),
+//     CLEAR_FILTER: ((state, payload ) => state.resourceFilter && { ...state, resourceFilter: null }),
+//     DAILY_VIEW: { ...state, view: 'DAILY' },
+//     WEEKLY_VIEW: { ...state, view: 'WEEKLY' }
+//   }
+
+//   return types[type]
+// }
